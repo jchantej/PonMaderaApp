cdbe21c399365f532a7af6bd5d9fa4180ab31ab7
Juan P. Jimenez <jchantej@gmail.com>
2023-02-24 09:58:01 -0500

    add component WebView

--- app/src/main/java/com/example/ponmaderaapp/MainActivity.java
+++ app/src/main/java/com/example/ponmaderaapp/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.ponmaderaapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        webView = (WebView)findViewById(R.id.activity_main_webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("https://job.ponmadera.com/trabajador");
+        webView.setWebViewClient(new WebViewClient());
     }
 }
