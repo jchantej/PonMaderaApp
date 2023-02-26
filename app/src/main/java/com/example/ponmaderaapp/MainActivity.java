e8a6cab394ba82fc084bed79485eb44c4a347d94
Juan P. Jimenez <jchantej@gmail.com>
2023-02-26 17:22:59 -0500

    set values credentials to form login

--- app/src/main/java/com/example/ponmaderaapp/MainActivity.java
+++ app/src/main/java/com/example/ponmaderaapp/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.ponmaderaapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Base64;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+
+import java.io.UnsupportedEncodingException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,7 +22,20 @@ public class MainActivity extends AppCompatActivity {
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webView.loadUrl("https://job.ponmadera.com/trabajador");
-        webView.setWebViewClient(new WebViewClient());
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public void onPageFinished(WebView web, String url) {
+                final String encodePassword = "Q2hhMTI3YWQxNDAxY2hhcG9u";
+                byte[] data = Base64.decode(encodePassword, Base64.DEFAULT);
+                try {
+                    final String password = new String(data, "UTF-8");
+                    webView.loadUrl("javascript:(function(){document.getElementById('inputEmail').value = 'jchantej@gmail.com';document.getElementById('inputPassword').value = '"+password+"';})()");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
     }
     @Override
